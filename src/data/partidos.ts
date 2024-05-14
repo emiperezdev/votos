0013af292c9d8e3fd429e9dd5cb9f8a2ea3be0ee
emiperezdev <emiperez.dev@gmail.com>
2024-05-14 17:35:55 -0600

    Modify candidates data

--- src/data/partidos.ts
+++ src/data/partidos.ts
@@ -4,50 +4,45 @@ import prd from "../assets/prd.png";
 import pri from "../assets/pri.png";
 import morena from "../assets/morena.png";
 import verde from "../assets/verde.png";
-
+import userGenerico from "../assets/user.jpg";
 
 const partidos: Partido[] = [
   {
     id: 1,
-    title: 'Partido de la educacion',
-    name: 'Jose Cruz Parada',
-    propuestas: ['a1a1a1a1', 'a1a1a1a1', 'a1a1a1a1', 'a1a1a1a1'],
+    title: "Partido progreso y justicia(PPJ)",
+    name: "Laura Fernandez",
+    propuestas: [
+      "Reforma educativa integral: Implementar un sistema educativo que fomente la creatividad, el pensamiento crítico y la educación tecnológica desde temprana edad.",
+      "Transparencia Gubernamental: Crear una plataforma digital que permita a los ciudadanos acceder a la información gubernamental en tiempo real, incluyendo presupuestos y gastos públicos.",
+      "Desarrollo Sostenible: Invertir en energías renovables y promover prácticas sostenibles en industrias clave para reducir la huella de carbono del país.",
+    ],
     votos: 0,
-    imagen: pan
+    imagen: userGenerico,
   },
   {
     id: 2,
-    title: 'Partido de los animales',
-    name: 'Aldo Murillo Perez',
-    propuestas: ['b2b2b2b2b2', 'b2b2b2b2b2', 'b2b2b2b2b2', 'b2b2b2b2b2'],
+    title: "Partido Unidad Nacional(PUN)",
+    name: "Carlos Mendez",
+    propuestas: [
+      "Fortalecimiento de la Seguridad: Aumentar el presupuesto para las fuerzas de seguridad, con énfasis en la capacitación y el equipamiento moderno para combatir el crimen organizado.",
+      "Infraestructura Moderna: Lanzar un programa nacional para la construcción y modernización de carreteras, puentes y redes de transporte público.",
+      "Apoyo a la Pequeña Empresa: Ofrecer incentivos fiscales y acceso a financiamiento a bajo interés para emprendedores y pequeñas empresas, fomentando la innovación y la creación de empleo.",
+    ],
     votos: 0,
-    imagen: prd
+    imagen: userGenerico,
   },
   {
     id: 3,
-    title: 'Partido de los deportes',
-    name: 'Emiliano Zapata Zanchez',
-    propuestas: ['c3c3c3c3', 'c3c3c3c3', 'c3c3c3c3', 'c3c3c3c3'],
+    title: "Partido Verde Futuro(PVF)",
+    name: "Ana Lopez",
+    propuestas: [
+      "Protección del Medio Ambiente: Implementar políticas estrictas para la conservación de bosques, ríos y áreas naturales protegidas, junto con un programa de reforestación masiva.",
+      "Salud y Bienestar: Aumentar la inversión en el sistema de salud pública para garantizar el acceso universal a servicios médicos de calidad, incluyendo la salud mental.",
+      "Economía Circular: Promover un modelo económico que reduzca el desperdicio a través del reciclaje, la reutilización y la innovación en materiales sostenibles.",
+    ],
     votos: 0,
-    imagen: morena
+    imagen: userGenerico,
   },
-  {
-    id: 4,
-    title: 'Partido de la comida',
-    name: 'Mauricio Castro Becerra',
-    propuestas: ['d4d4d4d4', 'd4d4d4d4', 'd4d4d4', 'd4d4d4d4'],
-    votos: 0,
-    imagen: pri
-  },
-  {
-    id: 5,
-    title: 'Partido del Trabajo',
-    name: 'Miguel Marquez Marquez',
-    propuestas: ['e5e5e5e5', 'e5e5e5e5', 'e5e5e5e5', 'e5e5e5e5'],
-    votos: 0,
-    imagen: verde
-  },
-  
 ];
 
 export default partidos;
